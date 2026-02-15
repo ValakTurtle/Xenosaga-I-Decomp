a392d5796d0a5152e84128acfe0672d74d373a56
Collin Hemze <71791303+ValakTurtle@users.noreply.github.com>
2026-02-14 22:22:32 -0600

    Initial decomp environment setup with Splat

---
+++ asm/data/cod/2ADE70.s
@@ -0,0 +1,9 @@
+.include "macro.inc"
+
+.section .data, "wa"
+
+nonmatching __cod_2ADE70_databin
+
+dlabel __cod_2ADE70_databin
+.incbin "assets/cod/2ADE70.databin.bin"
+enddlabel __cod_2ADE70_databin
